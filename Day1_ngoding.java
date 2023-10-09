2b4bdbe935b23143b61d0abe7e4b1bb1958cee52
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-09 16:22:47 +0800

    100 hari ngoding

---
+++ Day1_ngoding.java
@@ -0,0 +1,12 @@
+package day1_ngoding;
+
+public class Day1_ngoding {
+
+    public static void main(String[] args) {
+        String nama = "Rahmat Rahmadi";
+        int umur = 19;
+        System.out.println("Nama saya " + nama);
+        System.out.println("Umur " + umur + " tahun");
+    }
+
+}
