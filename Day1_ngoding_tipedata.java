4fadf0814f8d6efeba7b315113e566a2c2edbed8
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-09 16:47:10 +0800

    100 hari ngoding

---
+++ Day1_ngoding_tipedata.java
@@ -0,0 +1,26 @@
+package day1_ngoding;
+public class Day1_ngoding_tipedata {
+    public static void main(String[] args) {
+        String nama = "Rahmat Rahmadi";
+        String alamat = "Banua Baru";
+        int umur = 19;
+        String nim = "D0223327";
+        char kelas = 'C';
+        String prodi = "Informatika";
+        long angkatan = 2023;
+        float berat = 79f;
+        double tinggi = 178;
+        boolean belum_menikah = true;
+        System.out.println("Nama          : " + nama);
+        System.out.println("Alamat        : " + alamat);
+        System.out.println("Umur          : " + umur + " tahun");
+        System.out.println("NIM           : " + nim);
+        System.out.println("Kelas         : " + kelas);
+        System.out.println("Prodi         : " + prodi);
+        System.out.println("Angkatan      : " + angkatan);
+        System.out.println("Berat         : " + berat);
+        System.out.println("Tinggi        : " + tinggi);
+        System.out.println("Belum Menikah : " + belum_menikah);
+    }
+    
+}
