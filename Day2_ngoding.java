1fa747b7e5afd1e34584f8f71f2970bbad24856e
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-10 17:54:06 +0800

    100 hari ngoding

---
+++ Day2_ngoding.java
@@ -0,0 +1,21 @@
+package day1_ngoding;
+
+public class Day2_ngoding {
+//    Tipe data primitif dan non-primitif merupakan dua jenis tipe data dalam pemrograman
+//    Tipe data primitif adalah tipe data dasar yang menyimpan nilai tunggal
+//    Sedangkan tipe data non-primitif adalah tipe data yang dapat menyimpan beberapa nilai atau karakter
+
+    public static void main(String[] args) {
+//    Tipe data primitif
+        int umur = 19;
+//    Ini adalah contoh variabel dengan tipe data primitif Integer
+        System.out.println("Umur " + umur + " tahun");
+//    Ini untuk menampilkan nilai tipe data primitif
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+//    Tipe data non-primitif
+        String nama = "Rahmat Rahmadi";
+//    Ini adalah contoh variabel dengan tipe data non-primitif String
+        System.out.println("Nama : " + nama);
+//    Ini untuk menampilkan karakter tipe data primitif
+    }
+}
