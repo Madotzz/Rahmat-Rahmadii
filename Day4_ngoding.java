b6b8668987dcd9f78047b6f3de1d569b8372b140
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-12 20:15:58 +0800

    100 hari ngoding

---
+++ Day4_ngoding.java
@@ -0,0 +1,17 @@
+package day1_ngoding;
+
+import java.util.Scanner;
+
+public class Day4_ngoding {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan angka pertama : ");
+        int angka1 = input.nextInt();
+        System.out.println("Masukkan angka kedua : ");
+        int angka2 = input.nextInt();
+        System.out.println("Hasil : ");
+        System.out.println(angka1 + angka2);
+        
+                
+    }
+}
