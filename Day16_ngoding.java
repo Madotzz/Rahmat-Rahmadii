81c73f83ac8183504c71ffc870e827b11056f8ad
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-24 20:27:46 +0800

    Membuat segitiga menggunakan perulangan

---
+++ Day16_ngoding.java
@@ -0,0 +1,13 @@
+package day1_ngoding;
+public class Day16_ngoding {
+    public static void main(String[] args) {
+//  Berikut adalah cara membuat segitiga menggunakan perulangan for
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+    }
+}
