bf603636118ef31147b16340675b7aed224130a0
Rahmat Rahmadi <107305168+Madotzz@users.noreply.github.com>
2023-11-10 21:47:02 +0800

    Update Latihan Soal || Perulangan

---
+++ Day33_LatihanPerulangan.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //  Membuat program menampilkan angka genap 
+        int angka1 = 5;
+        int angka2 = 200;
+        for (int i = angka1; i < angka2; i++){
+            if (i % 2 == 0){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
