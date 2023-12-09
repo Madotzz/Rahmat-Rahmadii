139bb2c621e49fa945c9e1c345a98e514eb699f5
Madotzz <rahmatrahmadi920@gmail.com>
2023-12-09 21:02:43 +0800

    Membuat program method sederhana

---
+++ Day62_Method.java
@@ -0,0 +1,13 @@
+package day1_ngoding;
+public class Day62_Method {
+//  Membuat program method
+    static void biodata(){
+        System.out.println("Nama saya Rahmat Rahmadi");
+        System.out.println("Umur 19 tahun");
+        System.out.println("Tinggal di Campalagian");
+        System.out.println("Kuliah di Unsulbar");
+    }
+    public static void main(String[] args) {
+        biodata();
+    }
+}
