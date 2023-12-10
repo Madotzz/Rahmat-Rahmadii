7241d7c96d5a448e06db07decd3b4975f4501560
Rahmat Rahmadi <107305168+Madotzz@users.noreply.github.com>
2023-12-10 22:52:26 +0800

    belajar method

---
+++ Day63_Method.java
@@ -0,0 +1,19 @@
+package day1_ngoding;
+
+public class Day63_Method {
+    //prosedur
+    public static void isc(){
+        String kata = "Cara mendeklarasikan PROSEDUR";
+        System.out.println(kata);
+    }
+    //fungsi
+    public static String days(){
+        String kalimat = "Cara mendeklarasikan FUNGSI";
+        return kalimat;
+    }
+   
+     public static void main(String[] args) {
+        isc();
+         System.out.println(days());
+    }
+}
